2c6f8274d7448d447d3f4002bfb0deb5f4513eba
Eduardo Santos <eduardosantoshf@gmail.com>
2022-04-18 17:05:19 +0100

    Minor changes

--- problem2/main.c
+++ problem2/main.c
@@ -5,15 +5,97 @@
 #include <unistd.h>
 #include <string.h>
 
+
+static int process_command(int argc, char *argv[]);
+
 static void printUsage (char *cmdName);
 
 int main(int argc, char * argv[]) {
     double determinant;
     int i, j, k;
+    // will hold the output of processing the command
+    int command_result;
 
     double t0, t1, t2; /* time limits */
     t2 = 0.0;
 
+    // process the command and act according to it
+    command_result = process_command(argc, argv);
+    if (command_result != EXIT_SUCCESS)
+        return command_result;
+
+    printf("\n");
+
+    for(int counter = 2; counter < argc; counter++){
+        
+        FILE *fp = fopen(argv[counter],"rb");  // r for read, b for binary
+        printf("Processing file %s\n\n", argv[counter]);
+        
+        int amount = 0;
+        fread(&amount, sizeof(int), 1, fp);
+
+        int order = 0;
+        fread(&order, sizeof(int), 1, fp);
+
+        double matrix[order][order];
+        int l = 0;
+
+        while (fread(&matrix, sizeof(matrix), 1, fp)) {
+            double ratio;
+            double det = 1;
+
+            t0 = ((double) clock ()) / CLOCKS_PER_SEC;
+
+            /* Here we are using Gauss Elimination
+            Technique for transforming matrix to
+            upper triangular matrix */
+
+            /* Applying Gauss Elimination */        
+            for (i = 0; i < order; i++)
+            {
+                if (matrix[i][i] == 0.0)
+                {
+                    printf("Mathematical Error!");
+                    exit(0);
+                }
+                for (j = i + 1; j < order; j++)
+                {
+                    ratio = matrix[j][i] / matrix[i][i];
+
+                    for (k = 0; k < order; k++)
+                    {
+                            matrix[j][k] = matrix[j][k] - ratio * matrix[i][k];
+                    }
+                }
+            }
+
+            /* Finding determinant by multiplying
+            elements in principal diagonal elements */
+            for (i = 0; i < order; i++)
+            {
+                det = det * matrix[i][i];
+            }
+
+            printf("Processing matrix %d\n", l + 1);
+            printf("The determinant is %.3e \n", det);
+
+            t1 = ((double) clock ()) / CLOCKS_PER_SEC;
+            t2 += t1 - t0;
+            
+            l++;
+        }
+        printf("\n");
+
+        fclose(fp);
+    }
+
+    printf ("\nElapsed time = %.6f s\n", t2);
+
+    return 0;
+
+}
+
+static int process_command(int argc, char *argv[]) {
     int opt;                  /* selected option */
     char *fName = "no name";  /* file name (initialized to "no name" by default) */
     int val = -1;             /* numeric value (initialized to -1 by default) */
@@ -61,89 +143,7 @@ int main(int argc, char * argv[]) {
     for (o = 0; o < argc; o++)
         printf ("Word %d = %s\n", o, argv[o]);
 
-    printf("\n");
-
-    for(int counter = 2; counter < argc; counter++){
-        
-        FILE *fp = fopen(argv[counter],"rb");  // r for read, b for binary
-        printf("Processing file %s\n\n", argv[counter]);
-        
-        int amount = 0;
-        fread(&amount, sizeof(int), 1, fp);
-
-        int order = 0;
-        fread(&order, sizeof(int), 1, fp);
-
-        double matrix[order][order];
-        int l = 0;
-
-        while (fread(&matrix, sizeof(matrix), 1, fp)) {
-            double ratio;
-            double det = 1;
-
-            t0 = ((double) clock ()) / CLOCKS_PER_SEC;
-
-            /* Here we are using Gauss Elimination
-            Technique for transforming matrix to
-            upper triangular matrix */
-
-            /* Applying Gauss Elimination */        
-            for (i = 0; i < order; i++)
-            {
-                if (matrix[i][i] == 0.0)
-                {
-                    printf("Mathematical Error!");
-                    exit(0);
-                }
-                for (j = i + 1; j < order; j++)
-                {
-                    ratio = matrix[j][i] / matrix[i][i];
-
-                    for (k = 0; k < order; k++)
-                    {
-                            matrix[j][k] = matrix[j][k] - ratio * matrix[i][k];
-                    }
-                }
-            }
-            
-            /* Displaying upper triangular matrix */
-            /*
-            printf("\nUpper Triangular Matrix: \n");
-
-            for(i = 0; i < order; i++)
-            {
-                for(j = 0; j < order; j++)
-                {
-                    printf("%7.2f\t",matrix[i][j]);
-                }
-                printf("\n");
-            }
-            */
-
-            /* Finding determinant by multiplying
-            elements in principal diagonal elements */
-            for (i = 0; i < order; i++)
-            {
-                det = det * matrix[i][i];
-            }
-
-            printf("Processing matrix %d\n", l + 1);
-            printf("The determinant is %.3e \n", det);
-
-            t1 = ((double) clock ()) / CLOCKS_PER_SEC;
-            t2 += t1 - t0;
-            
-            l++;
-        }
-        printf("\n");
-
-        fclose(fp);
-    }
-
-    printf ("\nElapsed time = %.6f s\n", t2);
-
-    return 0;
-
+    return EXIT_SUCCESS;
 }
 
 static void printUsage (char *cmdName)
